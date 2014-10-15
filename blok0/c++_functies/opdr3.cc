afbb6b5fc65a8fb27c870dc1a1c58faa37c2612f
Stefan Hendriks <stefan.1995@live.nl>
2014-10-15 21:24:16 +0200

    test

--- blok0/c++_functies/opdr3.cc
+++ blok0/c++_functies/opdr3.cc
@@ -7,9 +7,7 @@ int hallo(char **x, int length) {
 	int max = 0;
 	for (int i = 0; i < length; i++) {
 		int xi = atoi(x[i]);
-		if (xi > max) {
-			max = xi;
-		}
+		max = xi > max ? xi : max;
 	}
 	return max;
 }
